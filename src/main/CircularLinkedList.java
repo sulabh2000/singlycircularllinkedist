5dcb43b1514fcd5560736e6a2705b640f78d72d4
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-13 10:20:31 +0530

    init commit

---
+++ src/main/CircularLinkedList.java
@@ -0,0 +1,39 @@
+package main;
+
+public class CircularLinkedList
+{
+    int size = 0;
+    Node head = null;
+    Node tail = null;
+    class Node
+    {
+        int data;
+        Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+        public Node(int data,Node next)
+        {
+            this.data=data;
+            this.next=next;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+    }
+
+}
