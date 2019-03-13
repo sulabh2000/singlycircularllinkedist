021da2430ca172b48a1579bffa5d5d348fc27907
sulabh2000 <bansalsulabh13@gmail.com>
2019-03-13 11:05:13 +0530

    add methods

--- src/main/CircularLinkedList.java
+++ src/main/CircularLinkedList.java
@@ -35,5 +35,53 @@ public class CircularLinkedList
             this.data = data;
         }
     }
+        public void print() {
+            if (size == 0) {
+                System.out.println("List is Empty");
+            } else {
+                Node temp = head;
+                do {
+                    System.out.println(" " + temp.data);
+                    temp = temp.next;
+                }
+                while (temp != head);
+                System.out.println("");
+            }
+        }
+        public void addNodeToHead(int data)
+        {
+            Node n = new Node(data);
+            if(size == 0)
+            {
+                head = n;
+                tail= n;
+                n.next = head;
+            }
+            else
+            {
+                Node temp = head;
+                n.next = temp;
+                head = n;
+                tail.next = head;
+            }
+            size++;
+        }
+        public void addNodeToTail(int data)
+        {
+            if(size==0)
+            {
+                addNodeToHead(data);
+            }
+            else
+            {
+                Node node = new Node
+            }
+        }
+
+
+    public static void main(String[] args) {
+        CircularLinkedList linkedList = new CircularLinkedList();
+        linkedList.print();
+    }
 
 }
